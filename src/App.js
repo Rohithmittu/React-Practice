ca8959969a8d190d09083e7fe9f4f105409f65f9
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-03-31 17:21:21 +0530

    completed start rating project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Accordian from './Components/Accordion';
 import RandomColor from './Components/Random-color';
+import StarRating from './Components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating />
     </div>
   );
 }
