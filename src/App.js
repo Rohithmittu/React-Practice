aaac99381e72c2222757ba3b1f61e6ead5acf0b5
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-04-02 13:44:23 +0530

    completed images slider project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import ImageSlider from './Components/Image-slider';
 // import Accordian from './Components/Accordion';
 // import RandomColor from './Components/Random-color';
 import StarRating from './Components/star-rating';
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
-      <StarRating />
+      {/* <StarRating /> */}
+      <ImageSlider  url={"https://picsum.photos/v2/list"} page={1} limit={10}/>
     </div>
   );
 }
